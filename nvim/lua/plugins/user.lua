cb44b36270e9660489729163f67dd5a98ce864c3
tylerscheuble <5944807+tylerscheuble@users.noreply.github.com>
2024-08-09 13:46:29 -0400

    fixes

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -19,20 +19,22 @@ return {
     opts = {
       -- Use shift-tab to toggle the floating terminal
       open_mapping = [[<S-Tab>]],
+      direction = "float",
+      auto_scroll = false,
     },
   },
   {
     "folke/which-key.nvim",
     opts = function(_, opts)
-      opts.triggers_blacklist = {
-        -- list of mode / prefixes that should never be hooked by WhichKey
-        -- this is mostly relevant for keymaps that start with a native binding
-        -- (tyler): I'm not sure what this does precisely, but it seems to be necessary for 
-        -- cutlass to work. See: https://github.com/gbprod/cutlass.nvim/issues/20#issuecomment-1713572459
-        i = { "j", "k", "d", "D", "s", "S" },
-        v = { "j", "k", "d", "D", "s", "S" },
-        n = { "d", "D", "s", "S" },
-      }
+      -- opts.triggers_blacklist = {
+      --   -- list of mode / prefixes that should never be hooked by WhichKey
+      --   -- this is mostly relevant for keymaps that start with a native binding
+      --   -- (tyler): I'm not sure what this does precisely, but it seems to be necessary for 
+      --   -- cutlass to work. See: https://github.com/gbprod/cutlass.nvim/issues/20#issuecomment-1713572459
+      --   i = { "j", "k", "d", "D", "s", "S" },
+      --   v = { "j", "k", "d", "D", "s", "S" },
+      --   n = { "d", "D", "s", "S" },
+      -- }
       return opts
     end,
   },
